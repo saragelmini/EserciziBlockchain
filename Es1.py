9b7bc122adc8f36ca54fa71fbd1ce2ede6d61dad
Sara Gelmini <69683185+saragelmini@users.noreply.github.com>
2021-10-14 23:17:46 +0200

    Add files via upload

--- Es1.py
+++ Es1.py
@@ -1,6 +1,7 @@
 import web3
 from Crypto.Hash import keccak
 from ProntoFunz import Prontohash
+import sys
 print ("import corretto")
 print ("---------------------------------------")
 
@@ -31,4 +32,59 @@ print ("---------------------------------------")
 if (k1 == k2 or k1 == k3 or k3 == k1):
     print ("Hashing failed")
 else :
-    print ("Hashing successful - all the stings are different")
+    print ("Hashing successful")
+
+#ricerca del nonce ad hoc 
+
+print ("---------------------------------------")
+print ('Verifica hash che iniziano per zero')
+#uno zero iniziale
+i=0
+while i>= 0:
+    if Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1]!= '0':
+        print ("Valore nonce: ",i)
+        print (Prontohash(stringa1+str(i)))
+        break
+    i =i+1
+#due zeri iniziali
+print ("---------------------------------------")
+i=0
+while i>= 0:
+    if Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1]== '0' and Prontohash(stringa1+str(i))[2]!= '0':
+        print ("Valore nonce: ",i)
+        print (Prontohash(stringa1+str(i)))
+        break
+    i =i+1
+#tre zeri iniziali
+print ("---------------------------------------")
+i=0
+while i>= 0:
+    if Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1]== '0' and Prontohash(stringa1+str(i))[2]=='0' and Prontohash(stringa1+str(i))[3]!= '0':
+        print ("Valore nonce",i)
+        print (Prontohash(stringa1+str(i)))
+        break
+    i =i+1
+
+print ("---------------------------------------")
+
+
+#conta di tutti i nonce per cui ho inizio con 0, 00 o 000 in intervallo i tra 0 e 3*10^7
+#inizializzo il contatore per gli zeri
+z1=1
+z2=1
+z3=1
+
+for i in range (0,30000000):
+   
+    if Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1]!= '0':
+        z1 = z1 + 1
+    elif Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1] == '0' and Prontohash(stringa1+str(i))[2]!= '0':
+        z2 = z2 + 1
+    elif Prontohash(stringa1+str(i))[0]== '0' and Prontohash(stringa1+str(i))[1]== '0' and Prontohash(stringa1+str(i))[2]== '0' and Prontohash(stringa1+str(i))[3]!= '0':
+        z3 = z3 + 1
+    
+
+print ('# di nonce per hash che inizia per 0: ',z1-1)
+print ('# di nonce per hash che inizia per 00: ',z2-1)
+print ('# di nonce per hash che inizia per 000: ',z3-1)
+
