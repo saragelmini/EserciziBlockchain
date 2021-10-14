c3045d4325a0dd0ea8e5e4c82031e9dd696d310c
Sara Gelmini <saragelmini@mbp-di-sara.homenet.telecomitalia.it>
2021-10-14 14:55:40 +0200

    modificato

--- Es1.py
+++ Es1.py
@@ -1,25 +1,22 @@
 import web3
 from Crypto.Hash import keccak
+from ProntoFunz import Prontohash
 print ("import corretto")
 print ("---------------------------------------")
 
 #test su prima stringa
 stringa1 = "Hello, World!"
 print(stringa1)
-
-k1 = keccak.new(digest_bits=256)
-k1.update(stringa1.encode("utf8"))
-print ("Il suo hash è: ",k1.hexdigest())
+k1 = Prontohash(stringa1)
+print ("Il suo hash è: ",k1)
 print ("---------------------------------------")
 
 
 #test su seconda stringa: stringa1 senza virgola 
 stringa2 = "Hello World!"
 print(stringa2)
-
-k2 = keccak.new(digest_bits=256)
-k2.update(stringa2.encode("utf8"))
-print ("Il suo hash è: ",k2.hexdigest())
+k2 = Prontohash(stringa2)
+print ("Il suo hash è: ",k2)
 print ("---------------------------------------")
 
 
@@ -27,10 +24,8 @@ print ("---------------------------------------")
 
 stringa3 = "Hello,  World!"
 print(stringa3)
-
-k3 = keccak.new(digest_bits=256)
-k3.update(stringa3.encode("utf8"))
-print ("Il suo hash è: ",k3.hexdigest())
+k3 = Prontohash(stringa3)
+print ("Il suo hash è: ",k3)
 print ("---------------------------------------")
 
 if (k1 == k2 or k1 == k3 or k3 == k1):
